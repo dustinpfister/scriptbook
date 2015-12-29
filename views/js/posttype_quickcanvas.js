c999ea21fac4c81d9200ab22ad59f2068de7b026
Dustin Pfister <dustin.pfister@gmail.com>
2015-12-29 16:25:28 -0500

    started work on the quickcanvas plugin

--- views/js/posttype_quickcanvas.js
+++ views/js/posttype_quickcanvas.js
@@ -3,3 +3,36 @@
     /html/frame_quick_canvas.html
 
 */
+
+postType.add((function(){
+
+    return {
+
+        postType: 'quickcanvas',
+
+        ui: function(){
+
+            return '<p>please make an interface for this posttype<\/p>';
+
+        },
+
+        onAction : {
+
+            ifClass_post_container : function(){
+
+                console.log('okay we have something good');
+
+            },
+
+            ifClass_quickcanvas_image_large: function(){
+
+                console.log('here i am');
+
+            }
+
+        },
+
+        postTemplate: function(postContent){ return '<p>blank<\/p>'; }
+        
+    }
+}()));
