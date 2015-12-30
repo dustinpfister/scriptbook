960d610ec987831f79d68820eeeb57693b701236
Dustin Pfister <dustin.pfister@gmail.com>
2015-12-30 13:00:59 -0500

    started working on updating wallpost_client_dialhome.js

--- views/js/wallpost_client_dialhome.js
+++ views/js/wallpost_client_dialhome.js
@@ -10,6 +10,52 @@
         console.log('dialing home...');
 
         // get the newest post id
+        var posts = _.get('wall_posts'),
+        latestID =  posts.children[0].id.replace(/post_container_/,''),
+        oldestID = posts.children[posts.children.length-1].id.replace(/post_container_/,'');
+    
+        myHttp.sendPostCheck(
+            {
+                checkType: 'newposts',
+                forUser: _.get('wall_username').innerHTML,
+                latestID: latestID,
+                oldestID: oldestID
+            }, 
+            function(res){
+
+                if(res.posts.length > 0 ){
+
+                    console.log('new posts!');
+
+                    //postType.injectpost(res.posts[0]);
+
+                }else{
+
+                    console.log('no new posts.');
+                }
+
+
+            }
+        );
+
+    };
+
+    postCheck();
+}());
+
+/*
+(function(){
+
+    //var get = function(id){ return document.getElementById(id)},
+
+    // post check thread
+    var postCheck = function(){
+
+        setTimeout(postCheck, 10000);
+
+        console.log('dialing home...');
+
+        // get the newest post id
         var posts = get('wall_posts'),
         latestID =  posts.children[0].id.replace(/post_container_/,''),
         oldestID = posts.children[posts.children.length-1].id.replace(/post_container_/,'');
@@ -42,3 +88,5 @@
 
     postCheck();
 }());
+
+*/
